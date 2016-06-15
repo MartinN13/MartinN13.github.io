7eaa9d1a680aa40bbf34f63dfeb630e8cc1ade35
MartinN13 <martinnabhan@me.com>
2016-06-15 17:04:08 +0200

    Fix main.js loading before page is ready

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,4 +1,4 @@
-(function(){
+$(document).ready(function() {
   $('.navbar-collapse').on('show.bs.collapse', function() {
     $('.navbar').css('background-image', 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(/assets/images/header.jpg)');
   });
@@ -6,4 +6,4 @@
   $('.navbar-collapse').on('hide.bs.collapse', function() {
     $('.navbar').css('background-image', '');
   });
-})();
+});
